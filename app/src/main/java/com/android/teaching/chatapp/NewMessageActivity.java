04127e6d7f3ca3bee29d71b2b8874d56a7e9f449
Franxu <franxu.11@hotmail.com>
2018-06-07 13:21:24 +0200

    se añade mas cositas..

--- app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
@@ -1,15 +1,49 @@
 package com.android.teaching.chatapp;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class NewMessageActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText messageEditText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        usernameEditText = findViewById(R.id.username);
+        messageEditText = findViewById(R.id.message);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        //Detectar si tenemos conectividad.
+        ConnectivityManager myConnectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo myNetworkInfo = myConnectivityManager.getActiveNetworkInfo();
+        boolean hasConnectivity = myNetworkInfo != null && myNetworkInfo.isConnectedOrConnecting();
+
+
+
+
+    }
+
+    public void onClick(View view) {
 
     }
 }
